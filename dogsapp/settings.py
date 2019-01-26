dfd2670676388913d9d80d0e97523e704552a28c
Todd Enders <tenders64@gmail.com>
2019-01-26 13:45:48 -0800

    postgres config

--- dogsapp/settings.py
+++ dogsapp/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
